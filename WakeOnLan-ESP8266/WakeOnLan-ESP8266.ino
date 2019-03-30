7959217d295bafee96797f33a60b4a0116911455
Brian Lough <ecenuig@gmail.com>
2019-03-30 00:04:35 +0000

    Initial wokring version

---
+++ WakeOnLan-ESP8266/WakeOnLan-ESP8266.ino
@@ -0,0 +1,153 @@
+/*******************************************************************
+    Send a WOL to a machine triggered from Telegram
+
+    By Brian Lough
+    YouTube: https://www.youtube.com/brianlough
+    Tindie: https://www.tindie.com/stores/brianlough/
+    Twitter: https://twitter.com/witnessmenow
+ *******************************************************************/
+#include <ESP8266WiFi.h>
+#include <WiFiClientSecure.h>
+#include <WiFiUDP.h>
+
+
+#include <WakeOnLan.h>
+// Library for sending the WOL magic packet
+// Needs to be installed from GitHub
+// https://github.com/koenieee/WakeOnLan-ESP8266
+
+#include <UniversalTelegramBot.h>
+// Library for interacting with the Telegram API
+// Search for "Telegegram" in the Library manager and install
+// The universal Telegram library
+// https://github.com/witnessmenow/Universal-Arduino-Telegram-Bot
+
+
+#include <ArduinoJson.h>
+// Library used for parsing Json from the API responses
+// NOTE: There is a breaking change in the 6.x.x version,
+// install the 5.x.x version instead
+// Search for "Arduino Json" in the Arduino Library manager
+// https://github.com/bblanchon/ArduinoJson
+
+//------- Replace the following! ------
+
+char ssid[] = "SSID";         // your network SSID (name)
+char password[] = "password"; // your network password
+
+#define TELEGRAM_BOT_TOKEN "12345679:XXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+
+/**
+ * The targets MAC address to send the packet to
+ */
+byte mac[] = { 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA };
+
+//------- ---------------------- ------
+
+WiFiUDP UDP;
+/**
+ * The Magic Packet needs to be sent as BROADCAST in the LAN
+ */
+IPAddress computer_ip(255,255,255,255); 
+
+void sendWOL();
+
+
+// This is the Wifi client that supports HTTPS
+WiFiClientSecure client;
+UniversalTelegramBot bot(TELEGRAM_BOT_TOKEN, client);
+
+int delayBetweenChecks = 1000;
+unsigned long lastTimeChecked;   //last time messages' scan has been done
+
+void setup() {
+  Serial.begin(115200);
+
+  // Set WiFi to station mode and disconnect from an AP if it was Previously connected
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+  delay(100);
+
+  // attempt to connect to Wifi network:
+  Serial.print("Connecting Wifi: ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  // Only required on 2.5 Beta
+  client.setInsecure();
+
+  
+  // longPoll keeps the request to Telegram open for the given amount of seconds if there are no messages
+  // This hugely improves response time of the bot, but is only really suitable for projects
+  // where the the initial interaction comes from Telegram as the requests will block the loop for
+  // the length of the long poll
+  bot.longPoll = 60;
+
+  UDP.begin(9);
+}
+
+void handleNewMessages(int numNewMessages) {
+
+  for (int i = 0; i < numNewMessages; i++) {
+
+    // If the type is a "callback_query", a inline keyboard button was pressed
+    if (bot.messages[i].type ==  F("callback_query")) {
+      String text = bot.messages[i].text;
+      Serial.print("Call back button pressed with text: ");
+      Serial.println(text);
+
+      if (text == F("WOL")) {
+        
+        WakeOnLan::sendWOL(computer_ip, UDP, mac, sizeof mac);
+      }
+    } else {
+      String chat_id = String(bot.messages[i].chat_id);
+      String text = bot.messages[i].text;
+
+      if (text == F("/options")) {
+
+        // Keyboard Json is an array of arrays.
+        // The size of the main array is how many row options the keyboard has
+        // The size of the sub arrays is how many coloums that row has
+
+        // "The Text" property is what shows up in the keyboard
+        // The "callback_data" property is the text that gets sent when pressed  
+        
+        String keyboardJson = F("[[{ \"text\" : \"Send Wake on Lan\", \"callback_data\" : \"WOL\" }]]");
+        bot.sendMessageWithInlineKeyboard(chat_id, "The options:", "", keyboardJson);
+      }
+
+      // When a user first uses a bot they will send a "/start" command
+      // So this is a good place to let the users know what commands are available
+      if (text == F("/start")) {
+
+        bot.sendMessage(chat_id, "/options : returns the inline keyboard\n", "Markdown");
+      }
+    }
+  }
+}
+
+void loop() {
+  if (millis() > lastTimeChecked + delayBetweenChecks)  {
+
+    // getUpdates returns 1 if there is a new message from Telegram
+    int numNewMessages = bot.getUpdates(bot.last_message_received + 1);
+
+    if (numNewMessages) {
+      Serial.println("got response");
+      handleNewMessages(numNewMessages);
+    }
+
+    lastTimeChecked = millis();
+  }
+}
